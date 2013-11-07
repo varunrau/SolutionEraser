30bdb1a56cac0695e84a5a2774aae4a7747d036b
varunrau <varunrau@gmail.com>
2013-11-07 13:37:59 -0800

    added argparse

--- remover.py
+++ remover.py
@@ -1,37 +1,50 @@
-
+import sys
+from argparse import ArgumentParser
 import math
 import Image
 
-img = Image.open("ee126.png")
-img = img.convert("RGBA")
-print img.getcolors()
-datas = img.getdata()
-red = (255, 0, 0)
-threshold = 250
+class AnswerRemover():
 
-def distance(first, second):
-	ret = 0
-	for index in range(len(first)):
-		ret += pow(first[index] - second[index], 2)
-	if first != (255, 255, 255):
-		#print str(math.sqrt(ret)) + "  " + str(first)
-		pass
-	return math.sqrt(ret)
+	def __init__(self, infile, outfile, threshold):
+		self.img = Image.open(infile).convert("RGBA")
+		self.outfile = outfile
+		self.threshold = threshold
+		self.red = (255, 0, 0)
+		self.data = self.img.getdata()
 
-def isRed(rgb):
-	return distance(rgb, red) < threshold
+	def isRed(self, rgb):
+		return self.distance(rgb, self.red) < self.threshold
 
 
-newData = []
-for item in datas:
-	if isRed(item[:3]):
-		newData.append((255, 255, 255, 255))
-	else:
-		newData.append(item)
+	def distance(self, first, second):
+		ret = 0
+		for index in range(len(first)):
+			ret += pow(first[index] - second[index], 2)
+		if first != (255, 255, 255):
+			#print str(math.sqrt(ret)) + "  " + str(first)
+			pass
+		return math.sqrt(ret)
 
-img.putdata(newData)
-img.save("ee126-converted.png", "PNG")
+	def processImage(self):
+		print "Processing..."
+		newData = []
+		for item in self.data:
+			if self.isRed(item[:3]):
+				newData.append((255, 255, 255, 255))
+			else:
+				newData.append(item)
+		self.img.putdata(newData)
+
+	def saveImage(self):
+		self.img.save(self.outfile, "PNG")
 
 if __name__ == "__main__":
-	pass
-	# do stuff
+	parser = ArgumentParser(description="first python version")
+	parser.add_argument('-i', '--infile', nargs='?', type=str, help='input file, in PNG format')
+	parser.add_argument('-o', '--outfile', nargs='?', type=str, default='out.png', help='output file, in PNG format')
+
+	args = parser.parse_args()
+	answer_remover = AnswerRemover(args.infile, args.outfile, 250)
+	answer_remover.processImage()
+	answer_remover.saveImage()
+
