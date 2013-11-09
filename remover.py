2bbd2fe292b3e65001657db21c393ae72dde3bbf
varunrau <varunrau@gmail.com>
2013-11-08 22:37:15 -0800

    changed threshold and red to black detector

--- remover.py
+++ remover.py
@@ -1,35 +1,38 @@
 import sys
 from argparse import ArgumentParser
 import math
+from wand.image import Image as Img
 import Image
 
 class AnswerRemover():
 
 	def __init__(self, infile, outfile, threshold):
-		self.img = Image.open(infile).convert("RGBA")
+		print infile
+		with Img(filename=infile, resolution=1000000) as image:
+			image.compression_quality = 100
+			image.save(filename="temp.png")
+		self.img = Image.open("temp.png").convert("RGBA")
 		self.outfile = outfile
 		self.threshold = threshold
-		self.red = (255, 0, 0)
+		self.black = (0, 0, 0)
 		self.data = self.img.getdata()
 
-	def isRed(self, rgb):
-		return self.distance(rgb, self.red) < self.threshold
+	def isBlack(self, rgb):
+		return self.distance(rgb, self.black) < self.threshold
 
 
 	def distance(self, first, second):
 		ret = 0
 		for index in range(len(first)):
 			ret += pow(first[index] - second[index], 2)
-		if first != (255, 255, 255):
-			#print str(math.sqrt(ret)) + "  " + str(first)
-			pass
 		return math.sqrt(ret)
 
 	def processImage(self):
 		print "Processing..."
+
 		newData = []
 		for item in self.data:
-			if self.isRed(item[:3]):
+			if not self.isBlack(item[:3]):
 				newData.append((255, 255, 255, 255))
 			else:
 				newData.append(item)
@@ -40,11 +43,11 @@ class AnswerRemover():
 
 if __name__ == "__main__":
 	parser = ArgumentParser(description="Removes all red from image")
-	parser.add_argument('-i', '--infile', nargs='?', type=str, help='input file, in PNG format')
-	parser.add_argument('-o', '--outfile', nargs='?', type=str, default='out.png', help='output file, in PNG format')
+	parser.add_argument('-i', '--infile', nargs='?', type=str, help='input file, in PDF format')
+	parser.add_argument('-o', '--outfile', nargs='?', type=str, default='out.png', help='output file, in PDF format')
 
 	args = parser.parse_args()
-	answer_remover = AnswerRemover(args.infile, args.outfile, 250)
+	answer_remover = AnswerRemover(args.infile, args.outfile, 100)
 	answer_remover.processImage()
 	answer_remover.saveImage()
 
